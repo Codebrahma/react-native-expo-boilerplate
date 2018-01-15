1bb9a67fffc8c756549614620aec8f49f84e7401
Kameshwaran <kamesh@Kameshs-MacBook-Pro.local>
2018-01-15 18:51:45 +0530

    Added proptypes and dispatched simple action

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { StyleSheet, Text, View } from 'react-native';
+import { connect } from 'react-redux';
 
 import globalStyles from './../styles/index';
 import { GREEN } from './../styles/colors';
+import { loadApp } from './../actions/app';
 
 const styles = StyleSheet.create({
   welcomeText: {
@@ -10,16 +13,30 @@ const styles = StyleSheet.create({
   },
 });
 
-const Home = () => (
-  <View style={[
-      globalStyles.container,
-      globalStyles.center,
-    ]}
-  >
-    <Text style={styles.welcomeText}>
-      Welcome! Your app loaded!
-    </Text>
-  </View>
-);
+class Home extends React.Component {
+  componentDidMount() {
+    this.props.loadApp();
+  }
 
-export default Home;
+  render() {
+    return (
+      <View style={[
+          globalStyles.container,
+          globalStyles.center,
+        ]}
+      >
+        <Text style={styles.welcomeText}>
+          Welcome! Your app loaded!
+        </Text>
+      </View>
+    );
+  }
+}
+
+Home.propTypes = {
+  loadApp: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = { loadApp };
+
+export default connect(null, mapDispatchToProps)(Home);
